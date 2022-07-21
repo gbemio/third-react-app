1ddcf67939affb342844599a8988b352c1b42ca9
Gbemisola Odukoya <folaryz@gmail.com>
2022-07-21 18:23:01 +0100

    Creating a webpage with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import MainContent from './MainContent.jsx';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <img src={logo} alt="" width="60px"  />
+//      <h1>Fun facts about React </h1>
+// <ol>
+//   <li>Was first released in 2013</li>
+//   <li>Was originally created by Jordan Walke</li>
+//   <li>Has well over 100k stars on Github</li>
+//   <li>Is maintained by Facebook</li>
+//   <li>Powers thousands of enterprise apps, including mobile apps</li>
+// </ol>
+//     </div>
+//   );
+// }
+
+// export default App;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <div> 
+   <Header/>
+   <MainContent/>
+   <Footer/>
+  </div>
+)
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg'
+
+const Header = () => {
+  return (
+    <header>
+    <nav>
+        <img src={logo} width="40px"alt="" />
+    </nav>
+</header>
+  )
+}
+
+export default Header
